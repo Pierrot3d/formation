965a1a9e761280fb17d0c6876fb6c0da89a241f6
Pierrot3d <pierre.baron37@orange.fr>
2023-02-22 09:28:11 +0100

    ajout de l'import de fichier excel

--- src/app/components/excel/excel.module.ts
+++ src/app/components/excel/excel.module.ts
@@ -2,21 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExcelComponent } from './excel/excel.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ImportExcelComponent } from './import-excel/import-excel.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 
 
 
 @NgModule({
   declarations: [
-    ExcelComponent
+    ExcelComponent,
+    ImportExcelComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   exports:
     [
-      ExcelComponent
+      ExcelComponent,
+      ImportExcelComponent
     ]
 })
 export class ExcelModule { }
