7353652652b088b23295592da38601553c2ff23e
PsimpleP <97801667+PsimpleP@users.noreply.github.com>
2023-03-11 16:09:58 +0100

    affichage couleurs en fonction des spécialités (plan de charge)

--- src/app/tabletest/tabletest.component.ts
+++ src/app/tabletest/tabletest.component.ts
@@ -10,29 +10,58 @@ export class TabletestComponent {
 
     {
       Avocat : 'personne 1' ,
-      NbrDeJourDeFormation : 6,
-      specialite : 'true'
+      NbrDeJourDeFormation : 8,
+      specialite : 'Mineurs'
     },
     {
       Avocat : 'personne 2' ,
-      NbrDeJourDeFormation : 4,
-      specialite : 'true'
-    },
-    {
-      Avocat : 'personne 3',
-      NbrDeJourDeFormation : 3,
-      specialite : 'false'
-  
-    },
-    {
-      Avocat : 'personne 4',
       NbrDeJourDeFormation : 6,
-      specialite : 'false'
-    }
+      specialite : 'Mineurs'
+    },
+    {
+      Avocat : 'personne 3' ,
+      NbrDeJourDeFormation : 9,
+      specialite : 'Pénal'
+    },
+    {
+      Avocat : 'personne 4' ,
+      NbrDeJourDeFormation : 4,
+      specialite : 'Pénal'
+    },
+    {
+      Avocat : 'personne 5',
+      NbrDeJourDeFormation : 2,
+      specialite : 'JLD-HO'
+    },
+    {
+      Avocat : 'personne 6',
+      NbrDeJourDeFormation : 4,
+      specialite : 'JLD-HO'
+    },
+    {
+      Avocat : 'personne 7',
+      NbrDeJourDeFormation : 6,
+      specialite : 'jeunes avocats'
+    },
+    {
+      Avocat : 'personne 8',
+      NbrDeJourDeFormation : 11,
+      specialite : 'jeunes avocats'
+    },
+    {
+      Avocat : 'personne 9',
+      NbrDeJourDeFormation : 11,
+      specialite : 'avocats spécialisés'
+    },
+    {
+      Avocat : 'personne 10',
+      NbrDeJourDeFormation : 6,
+      specialite : 'avocats spécialisés'
+    },
+    
+
     ]
-    Avocat: string;
-    NbrDeJourDeFormation : number;
-    specialite: string;
+
 }
 
 export class Tableau {
@@ -42,5 +71,6 @@ export class Tableau {
 }
 
 
+
   
-;
+
