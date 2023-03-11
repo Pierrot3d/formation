e53ba372ff3428ceda55994a05e33258d2952f19
PsimpleP <97801667+PsimpleP@users.noreply.github.com>
2023-03-11 12:47:33 +0100

    affichage du tableau

--- src/app/tabletest/tabletest.component.ts
+++ src/app/tabletest/tabletest.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { PauseOutlined } from '@material-ui/icons';
-import { FormExcel } from 'src/app/models/excel.model';
-import { ExcelService } from 'src/app/services/excel.service';
 
 @Component({
   selector: 'app-tabletest',
@@ -9,43 +6,38 @@ import { ExcelService } from 'src/app/services/excel.service';
   styleUrls: ['./tabletest.component.scss']
 })
 export class TabletestComponent {
+   tableau : Tableau[]= [
 
-}
-
-async function ajout_donnees(){
-  await Excel.run (async (context) => {
-    let feuille = context.workbook.worksheets.getActiveworksheets();
-    let tableaudonnees = feuille.tables.add('A1:D1', true);
-    tableaudonnees.name = 'tableaudonnees';
-    tableaudonnees.getHeaderrowrange().values = [['Avocat', 'NbrDeJourDeFormation', 'specialite']];
+    {
+      Avocat : 'personne 1' ,
+      NbrDeJourDeFormation : 6,
+      specialite : 'true'
+    },
+    {
+      Avocat : 'personne 2' ,
+      NbrDeJourDeFormation : 4,
+      specialite : 'true'
+    },
+    {
+      Avocat : 'personne 3',
+      NbrDeJourDeFormation : 3,
+      specialite : 'false'
   
-var data = [
-  {
-    Avocat : 'personne 1' ,
-    NbrDeJourDeFormation : 6,
-    specialite : 'true'
-  },
-  {
-    Avocat : 'personne 2' ,
-    NbrDeJourDeFormation : 4,
-    specialite : 'true'
-  },
-  {
-    Avocat : 'personne 3',
-    NbrDeJourDeFormation : 3,
-    specialite : 'false'
-
-  },
-  {
-    Avocat : 'personne 4',
-    NbrDeJourDeFormation : 6,
-    specialite : 'false'
-  }
-
-];
-let nouvellesdonnees = data.map((item) =>[item.Avocat , item.NbrDeJourDeFormation , item.specialite]);
-
-tableaudonnees.rows.add(null, nouvellesdonnees);
-
-});
+    },
+    {
+      Avocat : 'personne 4',
+      NbrDeJourDeFormation : 6,
+      specialite : 'false'
+    }
+    ]
 }
+
+export class Tableau {
+  Avocat: string;
+  NbrDeJourDeFormation : number;
+  specialite: string;
+}
+
+
+  
+;
